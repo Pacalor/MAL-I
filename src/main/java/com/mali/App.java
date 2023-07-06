adaec34705922ddc89ccc617534d5cfdc0de001e
Pacalor <26070953+Pacalor@users.noreply.github.com>
2023-07-06 13:41:23 +0000

    Create screenshot code

--- src/main/java/com/mali/App.java
+++ src/main/java/com/mali/App.java
@@ -1,5 +1,14 @@
 package com.mali;
 
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
 /**
  * Hello world!
  */
@@ -10,8 +19,27 @@ public final class App {
     /**
      * Says hello to the world.
      * @param args The arguments of the program.
+     * @throws AWTException
      */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws AWTException {
+        try {
+            // Create a Robot instance
+            Robot robot = new Robot();
+
+            // Get the screen size
+            Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+
+            // Capture the screen image
+            BufferedImage screenshot = robot.createScreenCapture(screenRect);
+
+            // Save the screenshot to a file
+            File output = new File("screenshot.png");
+            ImageIO.write(screenshot, "png", output);
+
+            System.out.println("Screenshot captured successfully!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
